4242e8573367dd891491c088782de6f66ddef406
reesi <=>
2025-09-09 09:33:30 +0400

    fix: added default no admin

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();

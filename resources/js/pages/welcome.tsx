9eb5a8791969ffb3c2adde8a7138892333f626bd
reesi <=>
2025-10-03 19:56:23 +0400

    ref: removed unwanted feature description

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -194,22 +194,6 @@ export default function Welcome() {
                                     </div>
                                     <div>
                                         <p className="text-sm font-medium text-[#1b1b18] dark:text-[#EDEDEC]">
-                                            Custom Short Links
-                                        </p>
-                                        <p className="text-xs text-[#6B7280] dark:text-[#9CA3AF]">
-                                            Create memorable, branded short links with custom aliases
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <div className="flex items-start space-x-3">
-                                    <div className="flex-shrink-0 w-5 h-5 rounded-full bg-green-100 dark:bg-green-900/30 flex items-center justify-center mt-0.5">
-                                        <svg className="w-3 h-3 text-green-600 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                        </svg>
-                                    </div>
-                                    <div>
-                                        <p className="text-sm font-medium text-[#1b1b18] dark:text-[#EDEDEC]">
                                             Link Moderation & Safety
                                         </p>
                                         <p className="text-xs text-[#6B7280] dark:text-[#9CA3AF]">
